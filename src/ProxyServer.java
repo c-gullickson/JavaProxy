05dea689bf30bec846aa89d47a8a861a4cfeb1a5
CGull2018 <38177880+CGull2018@users.noreply.github.com>
2022-02-20 12:02:03 -0600

    Build ProxyServer.java and RequestHandler.java classes with proxy logic

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -1,9 +1,11 @@
+
 //package Proxy;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -14,61 +16,98 @@ import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-
-
 public class ProxyServer {
 
-	//cache is a Map: the key is the URL and the value is the file name of the file that stores the cached content
-	Map<String, String> cache;
-	
-	ServerSocket proxySocket;
+	// cache is a Map: the key is the URL and the value is the file name of the file
+	// that stores the cached content
+	Map<String, File> cache;
 
-	String logFileName = "log.txt";
+	ServerSocket proxySocket;
+	Socket welcomSocket;
+	RequestHandler requestHandler;
+
+	File logDir = null;
+	String logFileName = "/log.txt";
+	File logFile = null;
+	PrintWriter logWriter;
 
 	public static void main(String[] args) {
-		new ProxyServer().startServer(Integer.parseInt(args[0]));
-		
+		// new ProxyServer().startServer(Integer.parseInt(args[0]));
+		// Using Hardcoded server port for testing
+		new ProxyServer().startServer(7777);
+
 	}
 
 	void startServer(int proxyPort) {
+		System.out.println("Starting Server on port: " + proxyPort);
+		System.out.println("Creating Cache: ");
 
+		// create a cache object
 		cache = new ConcurrentHashMap<>();
+		try {
+			// create the directory to store cached files.
+			File cacheDir = new File("cached");
+			if (!cacheDir.exists() || (cacheDir.exists() && !cacheDir.isDirectory())) {
+				cacheDir.mkdirs();
+				System.out.println("Creating Cached directory: ");
+			}
 
-		// create the directory to store cached files. 
-		File cacheDir = new File("cached");
-		if (!cacheDir.exists() || (cacheDir.exists() && !cacheDir.isDirectory())) {
-			cacheDir.mkdirs();
+			// create the directory to store a log file.
+			logDir = new File("log");
+			if (!logDir.exists() || (logDir.exists() && !logDir.isDirectory())) {
+				logDir.mkdirs();
+				System.out.println("Creating Log directory: ");
+			}
+			
+			logFile = new File(logDir + logFileName);
+			if (!logFile.exists() || (logFile.exists() && !logFile.isFile())) {
+				logFile.createNewFile();
+				System.out.println("Creating logWriter to file: " + logFile.getPath());
+			}
+			
+			logWriter = new PrintWriter(new FileWriter(logFile, true));
+			
+			System.out.println("Creating ServerSocket on proxy port: " + proxyPort);
+			proxySocket = new ServerSocket(proxyPort);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 
-		/**
-			 * To do:
-			 * create a serverSocket to listen on the port (proxyPort)
-			 * Create a thread (RequestHandler) for each new client connection 
-			 * remember to catch Exceptions!
-			 *
-		*/
- 
-		
+		// While the proxy server is running
+		// Need to change the flag from hardcoded true to a flaggable value
+		// Leave as true for now to have to manually close proxy exe
+		while (!proxySocket.isClosed()) {
+			try {
+				welcomSocket = proxySocket.accept();
+				System.out.println("Create welcome socket on Local Port: " + welcomSocket.getLocalPort());
+
+				requestHandler = new RequestHandler(welcomSocket, this);
+				Thread requestThread = new Thread(requestHandler);
+				requestThread.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
-
-
-	public String getCache(String hashcode) {
+	public File getCache(String hashcode) {
 		return cache.get(hashcode);
 	}
 
-	public void putCache(String hashcode, String fileName) {
+	public void putCache(String hashcode, File fileName) { 
+		System.out.println("PUT Cache Item hashcode: " + hashcode);
 		cache.put(hashcode, fileName);
 	}
 
-	public synchronized void writeLog(String info) {
-		
-			/**
-			 * To do
-			 * write string (info) to the log file, and add the current time stamp 
-			 * e.g. String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-			 *
-			*/
+	public synchronized void writeLog(String clientIpAddress, String url, String serverIpAddress) {
+
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		String log = timeStamp + " : " + clientIpAddress + " : " + url + " : " + serverIpAddress + "\n";
+
+		logWriter.append(log); 
+		logWriter.flush(); 
 	}
 
 }
--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -1,125 +1,288 @@
+
 //package Proxy;
 
 import java.io.*;
 import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.MessageDigest;
 import java.security.SecureRandom;
-
 
 // RequestHandler is thread that process requests of one client connection
 public class RequestHandler extends Thread {
 
-	
-	Socket clientSocket;
+	ProxyServer proxyServer = null; // create a reference to the ProxyServer class
+	Socket clientSocket = null; // create a socket from the client to the proxy
 
 	InputStream inFromClient;
-
 	OutputStream outToClient;
-	
-	byte[] request = new byte[1024];
+	BufferedReader clientToProxyBufferedReader;
+	BufferedWriter clientToProxyBufferedWriter;
 
-	BufferedReader proxyToClientBufferedReader;
+	Socket serverSocket = null; // create a socket from the proxy to the network server
+	InputStream inFromServer;
+	OutputStream outToServer;
+	BufferedReader proxyToServerBufferedReader;
+	BufferedWriter proxyToServerBufferedWriter;
 
-	BufferedWriter proxyToClientBufferedWriter;
+	String fileName = "";
 
-	
-	private ProxyServer server;
+	InetAddress serverIpAddress = null;
+	String clientIpAddress = null;
+	String serverIp = null;
 
+	String urlString = "";
+	String requestType = "";
+	int port = 80;
 
+	// Pass in socket object
 	public RequestHandler(Socket clientSocket, ProxyServer proxyServer) {
 
-		
+		System.out.println("Creating a new RequestHandler object by accepting a connection to the proxy server");
 		this.clientSocket = clientSocket;
-		
+		clientIpAddress = clientSocket.getInetAddress().toString();
 
-		this.server = proxyServer;
-
+		this.proxyServer = proxyServer;
 		try {
-			clientSocket.setSoTimeout(2000);
+			//clientSocket.setSoTimeout(5000);
 			inFromClient = clientSocket.getInputStream();
 			outToClient = clientSocket.getOutputStream();
 
+			fileName = generateRandomFileName() + ".dat";
+
 		} catch (Exception e) {
+			System.out.println(
+					"Error creating a new Request Handler and connection to the proxy server: " + e.toString());
 			e.printStackTrace();
 		}
-
 	}
 
-	
 	@Override
-	
+
 	public void run() {
 
-		/**
-			 * To do
-			 * Process the requests from a client. In particular, 
-			 * (1) Check the request type, only process GET request and ignore others
-			 * (2) If the url of GET request has been cached, respond with cached content
-			 * (3) Otherwise, call method proxyServertoClient to process the GET request
-			 *
-		*/
-
-	}
-
-	
-	private boolean proxyServertoClient(byte[] clientRequest) {
-
-		FileOutputStream fileWriter = null;
-		Socket serverSocket = null;
-		InputStream inFromServer;
-		OutputStream outToServer;
-		
-		// Create Buffered output stream to write to cached copy of file
-		String fileName = "cached/" + generateRandomFileName() + ".dat";
-		
-		// to handle binary content, byte is used
-		byte[] serverReply = new byte[4096];
-		
-			
-		/**
-		 * To do
-		 * (1) Create a socket to connect to the web server (default port 80)
-		 * (2) Send client's request (clientRequest) to the web server, you may want to use fluch() after writing.
-		 * (3) Use a while loop to read all responses from web server and send back to client
-		 * (4) Write the web server's response to a cache file, put the request URL and cache file name to the cache Map
-		 * (5) close file, and sockets.
-		*/
-		
-		return true;
-	}
-	
-	
-	
-	// Sends the cached content stored in the cache file to the client
-	private void sendCachedInfoToClient(String fileName) {
-
 		try {
+			// Data writing from the proxy to the client
+			clientToProxyBufferedWriter = new BufferedWriter(new OutputStreamWriter(outToClient));
 
-			byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+			// Data read from the client into the proxy
+			clientToProxyBufferedReader = new BufferedReader(new InputStreamReader(inFromClient));
 
-			outToClient.write(bytes);
-			outToClient.flush();
+			// Reading the HTTP request from the client that is connected to the proxy
+			// server
+			String clientRequest = clientToProxyBufferedReader.readLine();
+			System.out.println("Client Request String: " + clientRequest);
 
-		} catch (Exception e) {
-			e.printStackTrace();
+			if (clientRequest != null) {
+				// Get the Request type "CONNECT", "GET", "POST" from the client request
+				requestType = clientRequest.substring(0, clientRequest.indexOf(' '));
+
+//				// Get the urlString from the client request for where the message is destine to go
+				urlString = clientRequest.substring(clientRequest.indexOf(' ') + 1);
+//
+//				// Remove everything past next space
+				urlString = urlString.substring(0, urlString.indexOf(' '));
+//
+//				// Prepend http:// if necessary to create correct URL
+				if (!urlString.substring(0, 4).equals("http")) {
+					String temp = "http://";
+					urlString = temp + urlString;
+				}
+
+				// Split the current urlString into a basic url value, and host value
+				String url = urlString.substring(7);
+				String addressArray[] = url.split(":");
+				String host = addressArray[0];
+
+				// If the request is trying to establish an initial connection
+				// create a tunnel between the client and the network server
+				if (requestType.equals("CONNECT")) {
+
+					port = Integer.valueOf(addressArray[1]);
+					serverIpAddress = InetAddress.getByName(host);
+
+					// Create a new connection to the network server
+					ProxyToNetworkServerConnection(serverIpAddress, port, host);
+				} else {
+					System.out.println("Lookup in cache for host value: " + host);
+					if (!sendCachedInfoToClient(host)) {
+						// if there was not a cached file of the GET request, lookup the request using
+						// HTTP connections
+						sendUncachedRequestToServer(urlString, host);
+					}
+				}
+
+				if (serverIpAddress == null) {
+					serverIp = "";
+				} else {
+					serverIp = serverIpAddress.getHostAddress().toString();
+				}
+
+				/// Logs Ip of client + URL sent to proxy server
+				proxyServer.writeLog(clientIpAddress, url, serverIp);
+
+				/// Debug client request information
+				System.out.println("Request Type: " + requestType);
+				System.out.println("URL String: " + urlString);
+				System.out.println("URL: " + url);
+				System.out.println("Host: " + host);
+				System.out.println("Port: " + port);
+				System.out.println("Server Host IP Address " + serverIpAddress);
+			}
 		}
 
+		catch (UnknownHostException e) {
+			System.out.println(
+					"Unknown Host Exception error connecting to host: " + urlString + " Exception: " + e.toString());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Error on HTTPS : " + urlString + " Exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	private void ProxyToNetworkServerConnection(InetAddress serverIpAddress, int port, String host) {
+		try {
+			serverSocket = new Socket(serverIpAddress, port);
+
+			//serverSocket.setSoTimeout(5000);
+			inFromServer = serverSocket.getInputStream();
+			outToServer = serverSocket.getOutputStream();
+
+			String line = "HTTP/1.0 200 Connection established\r\n" + "Proxy-Agent: ProxyServer/1.0\r\n" + "\r\n";
+			clientToProxyBufferedWriter.write(line);
+			clientToProxyBufferedWriter.flush();
+
+			// Data writing to the network server
+			proxyToServerBufferedWriter = new BufferedWriter(new OutputStreamWriter(outToServer));
+
+			// Data read from the network server
+			proxyToServerBufferedReader = new BufferedReader(new InputStreamReader(inFromServer));
+
+			// Creating a separate runnable thread for the client to pass data to the
+			// network server
+			ClientConnectionToNetworkConnection clientToNetwork = new ClientConnectionToNetworkConnection(
+					clientSocket.getInputStream(), serverSocket.getOutputStream());
+			Thread clientToServerCommunication = new Thread(clientToNetwork);
+			clientToServerCommunication.start();
+
+			try {
+				byte[] buffer = new byte[4096];
+				int read = 0;
+				do {
+					read = serverSocket.getInputStream().read(buffer);
+					if (read > 0) {
+						clientSocket.getOutputStream().write(buffer, 0, read);
+						if (serverSocket.getInputStream().available() < 1) {
+							clientSocket.getOutputStream().flush();
+						}
+					}
+				} while (read >= 0);
+
+			} catch (SocketTimeoutException e) {
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	// Sends the cached content stored in the cache file to the client
+	private boolean sendCachedInfoToClient(String fileName) {
+
 		try {
 
-			if (clientSocket != null) {
-				clientSocket.close();
+			File cachedFile = proxyServer.getCache(fileName);
+			System.out.println("Cached Item from client: " + cachedFile); 
+			if (cachedFile != null) {
+				BufferedReader cachedFileBufferedReader = new BufferedReader(
+						new InputStreamReader(new FileInputStream(cachedFile)));
+
+//				String line = "HTTPS/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+//
+//				clientToProxyBufferedWriter.write(line);
+//				clientToProxyBufferedWriter.flush();
+
+				String cachedItem;
+				while ((cachedItem = cachedFileBufferedReader.readLine()) != null) {
+					System.out.println("Cached Item from client: " + cachedItem);
+					clientToProxyBufferedWriter.write(cachedItem);
+				}
+				
+				clientToProxyBufferedWriter.flush();
+				cachedFileBufferedReader.close();
+				clientToProxyBufferedWriter.close();
+
+				return true;
+			} else {
+				return false;
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-
+			return false;
 		}
 	}
-	
-	
-	// Generates a random file name  
+
+	// Sends the cached content stored in the cache file to the client
+	private void sendUncachedRequestToServer(String url, String host) {
+
+		try {
+
+			File cachedFile = null;
+			BufferedWriter cacheFileWriter = null;
+
+			cachedFile = new File("cached/" + fileName);
+
+			if (!cachedFile.exists()) {
+				cachedFile.createNewFile();
+			}
+
+			// Create Buffered output stream to write to cached copy of file
+			cacheFileWriter = new BufferedWriter(new FileWriter(cachedFile));
+
+			// Create a connection to remote server
+			URL remoteURL = new URL(url);
+			HttpURLConnection proxyToServerCon = (HttpURLConnection) remoteURL.openConnection();
+
+			BufferedReader proxyToServerReader = new BufferedReader(
+					new InputStreamReader(proxyToServerCon.getInputStream()));
+
+			String line = "HTTPS/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+			clientToProxyBufferedWriter.write(line);
+			clientToProxyBufferedWriter.flush();
+			
+			if (proxyToServerReader != null) {
+				// Read from input stream between proxy and remote server
+				while ((line = proxyToServerReader.readLine()) != null) {
+					// Send GET/POST data to client
+					clientToProxyBufferedWriter.write(line);
+					cacheFileWriter.write(line);
+				}
+				
+				clientToProxyBufferedWriter.flush();
+				cacheFileWriter.flush();
+
+
+				// push data file to proxy server cache
+				proxyServer.putCache(host, cachedFile);
+
+				if (cacheFileWriter != null) {
+					cacheFileWriter.close();
+				}
+				if (clientToProxyBufferedWriter != null) {
+					clientToProxyBufferedWriter.close();
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Load uncached item exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	// Generates a random file name for a cahced file
 	public String generateRandomFileName() {
 
 		String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
@@ -132,4 +295,37 @@ public class RequestHandler extends Thread {
 		return sb.toString();
 	}
 
+	public class ClientConnectionToNetworkConnection extends Thread {
+		InputStream inFromClientToServer;
+		OutputStream outFromServerToClient;
+
+		public ClientConnectionToNetworkConnection(InputStream inFromClientToServer,
+				OutputStream outFromServerToClient) {
+			this.inFromClientToServer = inFromClientToServer;
+			this.outFromServerToClient = outFromServerToClient;
+		}
+
+		@Override
+		public void run() {
+			try {
+				// Read byte by byte from client and send directly to server
+				byte[] buffer = new byte[4096];
+				int read;
+				do {
+					read = inFromClientToServer.read(buffer);
+					if (read > 0) {
+						outFromServerToClient.write(buffer, 0, read);
+						if (inFromClientToServer.available() < 1) {
+							outFromServerToClient.flush();
+						}
+					}
+				} while (read >= 0);
+
+			} catch (IOException e) {
+				System.out.println("Proxy to client HTTPS read Error");
+				e.printStackTrace();
+
+			}
+		}
+	}
 }
